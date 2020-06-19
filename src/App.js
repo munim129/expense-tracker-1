7440923a68c5c87dfbb4b8d273b5e339821ff70a
munim129 <munim129@users.noreply.github.com>
2020-06-19 19:23:55 +0500

    expense tracker app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Header } from './Components/Header'
+import { Balance } from './Components/Balance'
+import { AccountSummary } from './Components/AccountSummary'
+import { TransactionHistory } from './Components/TransactionHistory'
+import { AddTransaction } from './Components/AddTransaction'
+import {GlobalProvider} from './context/GlobalState'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+   <GlobalProvider>
+     <Header />
+     <div className="container" >
+       <Balance />
+       <AccountSummary />
+       <TransactionHistory />
+       <AddTransaction />
+     </div>
+     </GlobalProvider>
   );
 }
 
